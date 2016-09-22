9a09e9ff0791eddfae92a831ac9db581196f1a5f
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2016-09-22 19:55:40 +0100

    Implement centralised test scripts
    This patch implements test scripts intended for use by all
    OpenStack-Ansible role tests.
    
    The intent is to simplify the role tox.ini configuration
    and ensure that as many changes to role testing configuration
    can be managed from the centralised tests repo instead of
    individually in each repository.
    
    The common tests repo target location when cloned is changed
    from 'tests/playbooks' to 'tests/common' to better reflect
    the content and purpose.
    
    Change-Id: I1a93329ada24670c87588ce5a07f92c9bce4e50b

---
+++ test-bashate.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Copyright 2016, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# WARNING:
+# This file is use by all OpenStack-Ansible roles for testing purposes.
+# Any changes here will affect all OpenStack-Ansible role repositories
+# with immediate effect.
+
+# PURPOSE:
+# This script executes bashate against all the files it find that match
+# the search pattern. The search pattern is meant to find any shell
+# scripts present in the role.
+#
+# The test ignores the following rules:
+#
+# E003: Indent not multiple of 4 (we prefer to use multiples of 2)
+#
+# E006: Line longer than 79 columns (as many scripts use jinja
+#       templating, this is very difficult)
+#
+# E040: Syntax error determined using `bash -n` (as many scripts
+#       use jinja templating, this will often fail and the syntax
+#       error will be discovered in execution anyway)
+
+export WORKING_DIR=${WORKING_DIR:-$(pwd)}
+
+grep --recursive --binary-files=without-match \
+     --files-with-match '^.!.*\(ba\)\?sh$' \
+     --exclude-dir .tox \
+     --exclude-dir .git \
+     "${WORKING_DIR}" | xargs bashate --error . --verbose --ignore=E003,E006,E040
---
+++ test-log-collect.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Copyright 2016, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# WARNING:
+# This file is use by all OpenStack-Ansible roles for testing purposes.
+# Any changes here will affect all OpenStack-Ansible role repositories
+# with immediate effect.
+
+# PURPOSE:
+# This script collects, renames and compresses the logs produced in
+# a role test if the host is in OpenStack-CI.
+
+export WORKING_DIR=${WORKING_DIR:-$(pwd)}
+
+if [[ -d "/etc/nodepool" ]];then
+  mkdir -p "${WORKING_DIR}/logs/host" "${WORKING_DIR}/logs/openstack"
+  rsync --archive --verbose --safe-links --ignore-errors /var/log/ "${WORKING_DIR}/logs/host" || true
+  rsync --archive --verbose --safe-links --ignore-errors /openstack/log/ "${WORKING_DIR}/logs/openstack" || true
+  # Rename all files gathered to have a .txt suffix so that the compressed
+  # files are viewable via a web browser in OpenStack-CI.
+  find "${WORKING_DIR}/logs/" -type f -exec mv {} {}.txt \;
+  # Compress the files gathered so that they do not take up too much space.
+  # We use 'command' to ensure that we're not executing with some sort of alias.
+  command gzip --best --recursive "${WORKING_DIR}/logs/"
+fi

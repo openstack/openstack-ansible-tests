d2915ac9c76c0ecfb8d26febb29cceb149d83c3b
Jesse Pretorius <jesse.pretorius@rackspace.co.uk>
2017-06-24 12:51:03 +0100

    Update common repo files
    This patch updates the files which are commonly
    implemented across repositories. This includes
    an update to run_tests.sh which allows a selected
    test to be run instead of forcing all tests to
    be run. eg: ./run_tests.sh functional
    
    Change-Id: I8ff7b026289de5d01e5dade81d59fa6028d9eaf8

---
+++ run_tests_common.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+# Copyright 2015, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o pipefail
+set -euov
+
+BINDEP_FILE=${BINDEP_FILE:-bindep.txt}
+
+source /etc/os-release || source /usr/lib/os-release
+
+case "${ID,,}" in
+    centos)
+        sudo yum install -y python-devel redhat-lsb-core
+        ;;
+    ubuntu|debian)
+        sudo apt-get update && sudo apt-get install -y python-dev lsb-release
+        ;;
+    *)
+        echo "Unsupported distribution: ${ID,,}"
+        exit 1
+esac
+
+# Install pip
+if ! which pip &>/dev/null; then
+    curl --silent --show-error --retry 5 \
+        https://bootstrap.pypa.io/get-pip.py | sudo python2.7
+fi
+
+# Install bindep and tox
+sudo pip install 'bindep>=2.4.0' tox
+
+# CentOS 7 requires two additional packages:
+#   redhat-lsb-core - for bindep profile support
+#   epel-release    - required to install python-ndg_httpsclient/python2-pyasn1
+if [[ ${ID,,} == "centos" ]]; then
+    sudo yum -y install redhat-lsb-core epel-release
+fi
+
+# Get a list of packages to install with bindep. If packages need to be
+# installed, bindep exits with an exit code of 1.
+BINDEP_PKGS=$(bindep -b -f ${BINDEP_FILE} test || true)
+echo "Packages to install: ${BINDEP_PKGS}"
+
+# Install OS packages using bindep
+if [[ ${#BINDEP_PKGS} > 0 ]]; then
+    case "${ID,,}" in
+        centos)
+            sudo yum install -y $BINDEP_PKGS
+            ;;
+        ubuntu|debian)
+            sudo apt-get update
+            DEBIAN_FRONTEND=noninteractive \
+                sudo apt-get -q --option "Dpkg::Options::=--force-confold" \
+                --assume-yes install $BINDEP_PKGS
+            ;;
+    esac
+fi
+
+# Get envlist in a $env1,$env2,...,$envn format
+toxenvs="$(tox -l | tr '\n' ',' | sed 's/,$//')"
+
+# Execute all $toxenvs or only a specific one
+tox -e "${1:-$toxenvs}"

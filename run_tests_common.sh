9519c5bcfdaa5a4ca131111c4b2b76b183ecdcea
Major Hayden <major@mhtx.net>
2017-08-03 10:47:52 -0500

    Install yum-utils on Fedora
    This patch ensures that redhat-lsb-core and yum-utils are installed
    on Fedora systems. This is needed for bindep and yum-config-manager
    to work.
    
    This bug is blocking the ansible-hardening gate.
    
    Closes-Bug: 1708479
    Change-Id: I706b6f5f16ad7e6b86226830e2bceab2cda28227

--- run_tests_common.sh
+++ run_tests_common.sh
@@ -58,6 +58,8 @@ if [[ ${ID,,} == "centos" ]]; then
     # epel-release could be installed but not enabled (which is very common
     # in openstack-ci) so enable it here if needed
     sudo yum-config-manager --enable epel || true
+elif [[ ${ID,,} == "fedora" ]]; then
+    sudo dnf -y install redhat-lsb-core yum-utils
 # openSUSE 42.1 does not have python-ndg-httpsclient
 elif [[ ${ID,,} == *suse* ]] && [[ ${VERSION} == "42.1" ]]; then
     sudo pip install ndg-httpsclient

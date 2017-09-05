5da4a59b45554a18045ac6f33feb1e02b7f21c8c
Major Hayden <major@mhtx.net>
2017-09-05 14:40:17 -0500

    Backport /etc collection from pike
    This patch backports several patches that allow the gate jobs
    to collect data from /etc when a job completes.
    
    The SuSE and ARA bits are omitted as neither of those are
    applicable for Ocata.
    
    Change-Id: I41b5cd471fe22047f05f6bee5e27502bccd76963

--- test-log-collect.sh
+++ test-log-collect.sh
@@ -26,22 +26,47 @@
 ## Vars ----------------------------------------------------------------------
 
 export WORKING_DIR=${WORKING_DIR:-$(pwd)}
-export RSYNC_CMD="rsync --archive --verbose --safe-links --ignore-errors"
+export RSYNC_CMD="rsync --archive --safe-links --ignore-errors --quiet --no-perms --no-owner --no-group"
+export RSYNC_ETC_CMD="${RSYNC_CMD} --no-links --exclude selinux/"
 
 ## Main ----------------------------------------------------------------------
 
 if [[ -d "/etc/nodepool" ]]; then
-  mkdir -p "${WORKING_DIR}/logs/host" "${WORKING_DIR}/logs/openstack"
+
+  echo "#### BEGIN LOG COLLECTION ###"
+
+  mkdir -vp \
+    "${WORKING_DIR}/logs/host" \
+    "${WORKING_DIR}/logs/openstack" \
+    "${WORKING_DIR}/logs/etc/host" \
+    "${WORKING_DIR}/logs/etc/openstack" \
 
   # NOTE(mhayden): We use sudo here to ensure that all logs are copied.
   sudo ${RSYNC_CMD} /var/log/ "${WORKING_DIR}/logs/host" || true
-  sudo ${RSYNC_CMD} /openstack/log/ "${WORKING_DIR}/logs/openstack" || true
+  if [ -d "/openstack/log" ]; then
+    sudo ${RSYNC_CMD} /openstack/log/ "${WORKING_DIR}/logs/openstack" || true
+  fi
 
-  # NOTE(mhayden): All of the logs must be world-readable so that the log
+  # Archive host's /etc directory
+  sudo ${RSYNC_ETC_CMD} /etc/ "${WORKING_DIR}/logs/etc/host/" || true
+
+  # Loop over each container and archive its /etc directory
+  if which lxc-ls &> /dev/null; then
+    for CONTAINER_NAME in `sudo lxc-ls -1`; do
+      CONTAINER_PID=$(sudo lxc-info -p -n ${CONTAINER_NAME} | awk '{print $2}')
+      ETC_DIR="/proc/${CONTAINER_PID}/root/etc/"
+      sudo ${RSYNC_ETC_CMD} ${ETC_DIR} "${WORKING_DIR}/logs/etc/openstack/${CONTAINER_NAME}/" || true
+    done
+  fi
+
+  # NOTE(mhayden): All of the files must be world-readable so that the log
   # pickup jobs will work properly. Without this, you get a "File not found"
-  # when trying to read the logs in the job results.
+  # when trying to read the files in the job results.
+  # NOTE(odyssey4me): Using '--chown $(whoami) --chmod=ugo+rX' in the rsync
+  # CMD to achieve this would be optimal, but the CentOS version of rsync
+  # (3.0.x) does not support that option.
+  sudo chmod -R ugo+rX "${WORKING_DIR}/logs/"
   sudo chown -R $(whoami) "${WORKING_DIR}/logs/"
-  sudo chmod -R u+r,g+r,o+r "${WORKING_DIR}/logs/"
 
   if [ ! -z "${ANSIBLE_LOG_DIR}" ]; then
     mkdir -p "${WORKING_DIR}/logs/ansible"
@@ -51,7 +76,25 @@ if [[ -d "/etc/nodepool" ]]; then
   # Rename all files gathered to have a .txt suffix so that the compressed
   # files are viewable via a web browser in OpenStack-CI.
   find "${WORKING_DIR}/logs/" -type f ! -name '*.html' -exec mv {} {}.txt \;
+
+  # Get a dmesg output so we can look for kernel failures
+  dmesg > "${WORKING_DIR}/logs/dmesg.log.txt" || true
+
+  # output ram usage
+  free -m > "${WORKING_DIR}/logs/memory-available.txt" || true
+
+  # Redhat package debugging
+  if which yum &>/dev/null || which dnf &>/dev/null; then
+      # Prefer dnf over yum for CentOS.
+      which dnf &>/dev/null && RHT_PKG_MGR='dnf' || RHT_PKG_MGR='yum'
+      sudo $RHT_PKG_MGR repolist -v > "${WORKING_DIR}/logs/redhat-rpm-repolist.txt" || true
+      sudo $RHT_PKG_MGR list installed > "${WORKING_DIR}/logs/redhat-rpm-list-installed.txt" || true
+  fi
+
   # Compress the files gathered so that they do not take up too much space.
   # We use 'command' to ensure that we're not executing with some sort of alias.
   command gzip --best --recursive "${WORKING_DIR}/logs/"
+
+  echo "#### END LOG COLLECTION ###"
+
 fi

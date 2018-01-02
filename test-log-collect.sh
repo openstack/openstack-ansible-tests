1d24f2b4af5980e2e8cdb9588e2893080ac72b82
tonytan4ever <tony.tan@rackspace.com>
2018-01-02 12:25:01 -0500

    Skip and echo out gzip log compression error
    Sometimes gzip log collection will fail because certain linked
    log file are missing. This change notice those error and let the
    test pass
    
    Change-Id: I0720460dba3ec959429b577017713e1d5a209337
    (cherry picked from commit 6c800b5170c0b9065f7826acd2466bac4ea41c61)

--- test-log-collect.sh
+++ test-log-collect.sh
@@ -52,4 +52,6 @@ fi
 find "${WORKING_DIR}/logs/" -type f -exec mv {} {}.txt \;
 # Compress the files gathered so that they do not take up too much space.
 # We use 'command' to ensure that we're not executing with some sort of alias.
-command gzip --best --recursive "${WORKING_DIR}/logs/"
+command gzip --force --best --recursive "${WORKING_DIR}/logs/" || echo 'Note: gzip log files failed'
+
+echo "#### END LOG COLLECTION ###"

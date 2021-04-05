d88998cd686d67efc5b7ce3c23353ef2c257eefc
Dmitriy Rabotyagov <noonedeadpunk@ya.ru>
2021-04-05 11:48:48 +0300

    Bump ansible-lint and ansible version
    ansible-lint 5.0.9 has merged our NoLogPasswordRule [1]
    APTRepositoryCacheUpdateRule is not relevant since
    ansible fix has been merged for several years now.
    Also ansible-lint provides yamllint check.
    
    This means, we shouldn't really carry custom lint
    rules and can jsut use upstream ones.
    
    We also update ansible and collections version to
    match with the integrated repo.
    
    [1] https://github.com/ansible-community/ansible-lint/commit/8bef056d698f4cda64a52eec73fc9b6f70e40cb3
    
    Change-Id: Ie85d6d0a17849bbc665a7f49644c38b5f6a96bb2

--- test-ansible-lint.sh
+++ test-ansible-lint.sh
@@ -42,16 +42,8 @@ echo "ANSIBLE_LINT_PARAMS: ${ANSIBLE_LINT_PARAMS}"
 # Ensure that the Ansible environment is properly prepared
 source "${COMMON_TESTS_PATH}/test-ansible-env-prep.sh"
 
-# Run unit tests for OSA ansible-lint rules
-# Only do it if the repository being tested is the openstack-ansible-tests
-# repository.
-if [[ "$(basename ${WORKING_DIR})" == "openstack-ansible-tests" ]]; then
-  python -m unittest discover -s "${WORKING_DIR}/ansible-lint" -p 'Test*.py'
-fi
-
 # Execute ansible-lint. We do not want to test dependent roles located
 # in $HOME/.ansible/roles since we only care about the role we are currently
 # testing.
-ANSIBLE_LINT_WARNINGS="-w 204 -w 208 -w 306 -w metadata"
-ansible-lint ${ANSIBLE_LINT_PARAMS} ${ANSIBLE_LINT_WARNINGS} --exclude=$HOME/.ansible/roles \
-  -R -r ${COMMON_TESTS_PATH}/ansible-lint/ ${TEST_PLAYBOOK}
+ANSIBLE_LINT_WARNINGS="-w 208 -w 306 -w 502 -w 503 -w metadata"
+ansible-lint ${ANSIBLE_LINT_PARAMS} ${ANSIBLE_LINT_WARNINGS} --exclude=$HOME/.ansible/roles ${TEST_PLAYBOOK}
